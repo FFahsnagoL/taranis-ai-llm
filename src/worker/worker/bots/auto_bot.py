f43a9dc6ab9e3edc1b3cac4675641f9c7ca5c1f8
FFahsnagoL <59713343+FFahsnagoL@users.noreply.github.com>
2026-01-25 17:12:03 +0100

    Refactor AutoBot's analyze_news method
    Removed unused keywords from whitelist and updated the logic for analyzing news items.

--- src/worker/worker/bots/auto_bot.py
+++ src/worker/worker/bots/auto_bot.py
@@ -40,45 +40,7 @@ class AutoBot(BaseBot):
         results = {}
 
         whitelist = [
-        "Mac Os",
-        "macOS",
-        "mac OS"
-        "Windows 10",
-        "Windows 11",
-        "SharePoint",
-        "Ubuntu",
-        "RedHat",
-        "Red Hat",
-        "FreeBSD",
-        "iOS",
-        "iOS",
-        "Chrome",
-        "Firefox",
-        "Edge",
-        "Safari",
-        "WSUS",
-        "System Center",
-        "AOS(Alcatel)",
-        "FortiOS",
-        "Apache",
-        "Tomcat",
-        "IIS",
-        "BIND",
-        "Infoblox NIOS",
-        "Microsoft Active Directory",
-        "Active Directory",
-        "Postfix",
-        "Microsoft Exchange",
-        "Exchange",
-        "Sweden",
-        "Swedish",
-        "Scandi",
-        "Scandinavia",
-        "nordic",
-        "universit",
-        "university"
-        "universities",
-        "higher education"
+
         ]
         blacklist = [
             "Weekly Recap",
@@ -384,4 +346,5 @@ class AutoBot(BaseBot):
             #story["checked"] = True
             #self.add_or_update_story(story)
             # 
+
             # 
